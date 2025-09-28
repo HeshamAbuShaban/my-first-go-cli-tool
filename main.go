514f7150d4ef8995fa1802026ca556e7069749f9
HP <heshamabushaban.developer@gmail.com>
2025-09-28 23:22:32 +0300

    Add global --help with usage and subcommand list

--- main.go
+++ main.go
@@ -5,23 +5,40 @@ import (
   "os"
 )
 
+func printGlobalHelp() {
+    fmt.Println("Usage: mycli <command> [options]")
+    fmt.Println("\nAvailable commands:")
+    fmt.Println("  greet   Print a greeting with optional flags")
+    fmt.Println("  sum     Add numbers together")
+    fmt.Println("  time    Show the current time")
+    fmt.Println("\nUse \"mycli <command> --help\" for more information about a command.")
+}
+
 func main() {
-  if len(os.Args) < 2 {
-	fmt.Println("Usage: mycli <command> [args...]")
-	return
-  }
+	if len(os.Args) < 2 {
+        printGlobalHelp()
+        return
+	}
 
-  cmd := os.Args[1]
-  args := os.Args[2:]
+    // Global help
+    if os.Args[1] == "--help" || os.Args[1] == "-h" {
+        printGlobalHelp()
+        return
+    }
 
-  switch cmd {
-  case "greet":
-	  cmdGreet(args)
-  case "sum":
-	  cmdSum(args)
-  case "time":
-	  cmdTime(args)
-  default:
-	  fmt.Println("Unknown command:", cmd)
-  }
+
+    cmd := os.Args[1]
+    args := os.Args[2:]
+
+    switch cmd {
+    case "greet":
+        cmdGreet(args)
+    case "sum":
+        cmdSum(args)
+    case "time":
+        cmdTime(args)
+    default:
+        fmt.Println("Unknown command:", cmd)
+        printGlobalHelp()
+    }
 }
