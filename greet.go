8fec9cbd836ed08dcdb4027bbd984a24adeb2795
HP <heshamabushaban.developer@gmail.com>
2025-09-29 04:52:27 +0300

    Add flags to greet subcommand (--name, --shout)

--- greet.go
+++ greet.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+    "fmt"
+    "strings"
+)
 
 // it prints "Hello <name>!"
-
+// cmdGreet handles the "greet" subcommand with flags
 func cmdGreet(args []string) {
-	name := "World"
-	if len(args) > 0 {
-		name = args[0]
-	}
-	fmt.Printf("Hello %s!\n", name)
+    // Create a new FlagSet just for "greet"
+    greetCmd := flag.NewFlagSet("greet", flag.ExitOnError)
+
+    name := greetCmd.String("name", "World", "The name to greet")
+    shout := greetCmd.Bool("shout", false, "Whether to shout the greeting")
+
+	// Override the default usage message
+    greetCmd.Usage = func() {
+        fmt.Println("Usage: mycli greet [options]")
+        fmt.Println("\nOptions:")
+        greetCmd.PrintDefaults()
+    }
+
+    // Parse only the args passed after "greet"
+    err := greetCmd.Parse(args)
+    if err != nil {
+        return
+    }
+	
+	greeting := fmt.Sprintf("Hello %s!", *name)
+    
+	if *shout {
+        greeting = strings.ToUpper(greeting)
+    }
+    fmt.Println(greeting)
 }
+
