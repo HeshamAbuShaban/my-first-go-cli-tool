37de2daf7b546f1c35dfa1321d1f52bb5ef65624
HP <heshamabushaban.developer@gmail.com>
2025-09-29 05:30:45 +0300

    Add flags to time subcommand (--utc, --format)

--- time.go
+++ time.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-  "fmt"
-  "time"
+    "flag"
+    "fmt"
+    "time"
 )
 
-// this is souppose to show the current time!
-
+// cmdTime handles the "time" subcommand with flags
 func cmdTime(args []string) {
-	fmt.Println(time.Now().Format(time.RFC3339))
+    timeCmd := flag.NewFlagSet("time", flag.ExitOnError)
+
+    utc := timeCmd.Bool("utc", false, "Show time in UTC instead of local")
+    format := timeCmd.String("format", "15:04:05", "Time format (Go layout)")
+
+    // Custom usage
+    timeCmd.Usage = func() {
+        fmt.Println("Usage: mycli time [options]")
+        fmt.Println("\nOptions:")
+        timeCmd.PrintDefaults()
+        fmt.Println("\nFormat uses Go's time layout, e.g.:")
+        fmt.Println("  2001-09-11 08:44:05   -> full date/time")
+        fmt.Println("  15:04                 -> hours:minutes")
+    }
+
+    err := timeCmd.Parse(args)
+    if err != nil {
+        return
+    }
+
+    now := time.Now()
+    if *utc {
+        now = now.UTC()
+    }
+
+    fmt.Println(now.Format(*format))
 }
+
